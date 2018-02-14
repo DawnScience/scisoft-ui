e044be7e39cf36fc40b5f2419c5e06c27724483e
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-02-14 14:17:24 +0000

    SCI-6471: move PlotModeDataTable(1D|2D) to uk.ac.diamond.scisoft.analysis.rcp

---
+++ uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/plotmodes/PlotModeDataTable2D.java
@@ -0,0 +1,39 @@
+package uk.ac.diamond.scisoft.analysis.rcp.plotmodes;
+
+import org.dawnsci.datavis.model.PlotModeImage;
+import org.eclipse.dawnsci.plotting.api.IPlottingSystem;
+import org.eclipse.dawnsci.plotting.api.trace.ITableDataTrace;
+import org.eclipse.dawnsci.plotting.api.trace.ITrace;
+import org.eclipse.january.dataset.IDataset;
+
+public class PlotModeDataTable2D extends PlotModeImage {
+
+	@Override
+	public String[] getOptions() {
+		return options;
+	}
+
+	@Override
+	public String getName() {
+		return "Text Table 2D";
+	}
+
+	@Override
+	public boolean isThisMode(ITrace trace) {
+		return trace instanceof ITableDataTrace;
+	}
+	
+	@Override
+	public void displayData(IDataset[] data, ITrace[] update, IPlottingSystem<?> system, Object userObject) throws Exception {
+		IDataset d = data[0];
+		
+		
+		ITableDataTrace trace = new TableDataTrace2D(d);
+
+		trace.setUserObject(userObject);
+		system.addTrace(trace);
+		
+		
+	}
+
+}
