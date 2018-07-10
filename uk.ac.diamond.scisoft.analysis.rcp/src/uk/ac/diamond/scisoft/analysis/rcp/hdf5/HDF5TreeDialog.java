06e93661011b90b9f6ee2a14f77cec059d379266
Jacob Filik <jacob.filik@diamond.ac.uk>
2018-07-10 09:51:11 +0100

    SCI-7197 add the full path to the tree node to the description
    This allows it to be copied into other applications/scripts
    
    Change-Id: I191bd758d707941e69f470a884e6433d2d5f3ee3

--- uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/hdf5/HDF5TreeDialog.java
+++ uk.ac.diamond.scisoft.analysis.rcp/src/uk/ac/diamond/scisoft/analysis/rcp/hdf5/HDF5TreeDialog.java
@@ -26,6 +26,7 @@ import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.TreePath;
 import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
@@ -38,26 +39,25 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.PlatformUI;
 
 public class HDF5TreeDialog extends Dialog {
 
 	private Tree tree;
 	private String filename;
-	
+
 	protected HDF5TreeDialog(Shell parentShell, Tree tree, String filename) {
 		super(parentShell);
 		this.tree = tree;
 		this.filename = filename;
 	}
-	
+
 	@Override
 	public Control createDialogArea(Composite parent)  {
 		Composite container = (Composite) super.createDialogArea(parent);
 		container.setLayout(new FillLayout());
 		SashForm sash = new SashForm(container, SWT.HORIZONTAL);
- 		HDF5TableTree tt = new HDF5TableTree(sash, null, null, null);
+		HDF5TableTree tt = new HDF5TableTree(sash, null, null, null);
 		tt.setFilename(filename);
 		tt.setInput(tree.getNodeLink());
 
@@ -66,17 +66,25 @@ public class HDF5TreeDialog extends Dialog {
 		final StyledText l = new StyledText(c, SWT.WRAP | SWT.READ_ONLY | SWT.BORDER | SWT.V_SCROLL);
 		l.setAlwaysShowScrollBars(false);
 		l.setLayoutData(new GridData(GridData.FILL_BOTH));
-		
+
 		tt.getViewer().addSelectionChangedListener(new ISelectionChangedListener() {
-			
+
 			@Override
 			public void selectionChanged(SelectionChangedEvent event) {
 				ISelection selection = event.getSelection();
-				
+
 				StringBuilder b = new StringBuilder(event.getSelection().toString());
-				
+
 				if (selection instanceof TreeSelection && ((TreeSelection)selection).getFirstElement() instanceof NodeLink) {
 					NodeLink n = (NodeLink)((TreeSelection)selection).getFirstElement();
+					TreePath path = ((TreeSelection)selection).getPaths()[0];
+
+					String fullPath = getStringFromPath(path);
+
+					b.append(System.lineSeparator());
+					b.append("Path: ");
+					b.append(fullPath);
+
 					Node d = n.getDestination();
 					if (d instanceof DataNode) {
 						DataNode dn = (DataNode)d;
@@ -88,15 +96,26 @@ public class HDF5TreeDialog extends Dialog {
 						b.append(dn.getMaxShape() == null ? "Not set" : Arrays.toString(dn.getMaxShape()));
 					}
 				}
-				
+
 				l.setText(b.toString());
-				
+
 			}
 		});
-		
+
 		sash.setWeights(new int[] {70,30});
-		
+
 		return container;
+	}
+
+	private String getStringFromPath(TreePath path) {
+		StringBuilder pathBuilder = new StringBuilder();
+		for (int i = 0; i < path.getSegmentCount(); i++) {
+			pathBuilder.append(Node.SEPARATOR);
+			String segment = ((NodeLink)path.getSegment(i)).getName();
+			pathBuilder.append(segment);
+		}
+
+		return pathBuilder.toString();
 	}
 
 	@Override
