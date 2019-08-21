ba9650dddbfe682ded0b357c0bca34dabf7187bc
Peter Chang <peter.chang@diamond.ac.uk>
2019-08-21 16:52:25 +0100

    Fix Quick RIXS and Post RIXS
    Add injected method to override file controller so that file states are listened to by the analyser
    
    Change-Id: I49cd6ae8cfe44910cc898901642a4815c8b6b6e1

--- uk.ac.diamond.scisoft.rixs.rcp/src/uk/ac/diamond/scisoft/rixs/rcp/view/QRLoadedFilePart.java
+++ uk.ac.diamond.scisoft.rixs.rcp/src/uk/ac/diamond/scisoft/rixs/rcp/view/QRLoadedFilePart.java
@@ -1,10 +1,19 @@
 package uk.ac.diamond.scisoft.rixs.rcp.view;
 
+import javax.inject.Inject;
+
 import org.dawnsci.datavis.view.parts.LoadedFilePart;
 
 import uk.ac.diamond.scisoft.rixs.rcp.QuickRIXSPerspective;
 
 public class QRLoadedFilePart extends LoadedFilePart {
+
+	@Inject
+	protected void setQRFileController(IQRFileController fileController) {
+		// this is called after the superclass gets its fields injected
+		// so will override the standard one
+		setFileController(fileController);
+	}
 
 	@Override
 	protected String getPerspectiveID() {
