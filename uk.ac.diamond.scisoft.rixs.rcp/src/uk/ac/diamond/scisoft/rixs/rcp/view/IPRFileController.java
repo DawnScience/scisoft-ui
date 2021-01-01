d81fbdd00037556ee48643caf67a63b89d71652d
Peter Chang <peter.chang@diamond.ac.uk>
2021-01-01 17:54:10 +0000

    Remove PostRIXS perspective
    Moved to a tab in DataVis
    
    Change-Id: I92a0f5fb5665af0575da346766a8aae36016c1ec

--- uk.ac.diamond.scisoft.rixs.rcp/src/uk/ac/diamond/scisoft/rixs/rcp/view/IPRFileController.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.diamond.scisoft.rixs.rcp.view;
-
-import org.dawnsci.datavis.model.IFileController;
-
-public interface IPRFileController extends IFileController {
-
-}
--- uk.ac.diamond.scisoft.rixs.rcp/src/uk/ac/diamond/scisoft/rixs/rcp/view/PRFileController.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.diamond.scisoft.rixs.rcp.view;
-
-import org.dawnsci.datavis.model.FileController;
-
-public class PRFileController extends FileController implements IPRFileController {
-
-}
